24f6fd77653cf004706072cffec4e1d4310471a3
NadiaEl88 <144838255+NadiaEl88@users.noreply.github.com>
2024-02-12 07:14:04 +0100

    Update AppBuch.java

--- AppBuch.java
+++ AppBuch.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class AppBuch {
 
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in); 
+                
             Autor autor1 = new Autor("Arthur Conan Doyle");
             autor1.setSynonym("ACD");
             autor1.setGage(500);
@@ -14,10 +18,6 @@ public class AppBuch {
             autor3.setGage(750);
 
 
-
-
-
-
             Buch buch1 = new Buch("Der schwarze Peter",autor1, 9783841209566L, 0.99);
             Buch buch2 = new Buch("Der Junge, der vom Frieden träumte", autor2, 9783104033921L, 12);
             Buch buch3 = new Buch("Der Urlaub", autor3, 101837903719L, 10.99);
@@ -25,6 +25,27 @@ public class AppBuch {
             System.out.println(buch1);
             System.out.println(buch2);
             System.out.println(buch3);
-        }
+
+        System.out.println("Willkommen zum Buchverwaltungsprogramm!");
+
+        // Eingaben von der Konsole lesen
+        System.out.print("Titel eingeben: ");
+        String titel = scanner.nextLine();
+
+        System.out.print("Autor eingeben: ");
+        String autor = scanner.nextLine();
+
+        System.out.print("ISBN eingeben: ");
+        long isbn = scanner.nextLong();
+
+        System.out.print("Preis eingeben: ");
+        double preis = scanner.nextDouble();
+
+        // Ein Buch erstellen
+        Buch buch = new Buch(titel, new Autor(autor), 0, 0);
+        System.out.println("Buch erstellt: " + buch);
+        scanner.close();
+
     }
+}
 
